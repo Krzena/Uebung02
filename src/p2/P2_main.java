e2429875f319dce00c18f5d0c2a9d3f50c15a95e
Krzena <kriuchkova541@gmail.com>
2025-10-30 20:41:44 +0100

    added P2

---
+++ src/p2/P2_main.java
@@ -0,0 +1,15 @@
+package p2;
+
+public class P2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i = 15; 
+boolean b = true;
+
+if ( i > 10) { b = false;
+	}
+System.out.println("i hat den Wert" + i );
+System.out.println("b hat den Wert" + b);
+}
+}
