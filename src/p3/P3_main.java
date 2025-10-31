2d0041eb73016cf706272703d28b04ae681dfdf5
Krzena <kriuchkova541@gmail.com>
2025-10-31 01:12:57 +0100

    added P3

---
+++ src/p3/P3_main.java
@@ -0,0 +1,19 @@
+package p3;
+
+public class P3_main {
+
+	public static void main(String[] args) {
+
+		int i = 3;
+		int j = 10;
+		int k = 17;		
+
+if (i < j) { System.out.print ("A");}
+if (i < k) { System.out.print ("B");}
+if (j < k) { System.out.print ("C");}	
+
+}
+	}
+
+
+
