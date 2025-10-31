74fee2c0ab6a6874bdebfc06ab3e239839c0c10a
Krzena <kriuchkova541@gmail.com>
2025-10-31 02:42:18 +0100

    added h2

---
+++ src/h2/H2_main.java
@@ -0,0 +1,24 @@
+package h2;
+
+public class H2_main {
+
+	public static void main(String[] args) {
+		
+		int i = 100;
+		int j = 26;
+		int k = 44;
+		int min = i;
+		int max = i;
+		
+		if (j < min) {min = j;}
+		if (k < min) {min = k;}
+		
+		if (j > max) {max = j;}
+		if (k > max) {max = k;}
+		
+ 
+ System.out.println ("Minimum hat den Wert" + min);
+ System.out.println ("Maximum hat den Wert" + max);
+	}
+
+}
